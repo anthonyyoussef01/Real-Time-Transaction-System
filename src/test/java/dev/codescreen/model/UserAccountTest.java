73e0577936f7b027347890f7485d515f799738d8
antho <anthonyyoussef01@gmail.com>
2024-05-06 16:12:55 -0400

    Restructuring Update:
    - Assured all tests in TransactionEventTest are passing and work as expected
    - Added the currency to the UserAccounts
    - and added the messageId and currency to TransactionEvent

--- src/test/java/dev/codescreen/model/UserAccountTest.java
+++ src/test/java/dev/codescreen/model/UserAccountTest.java
@@ -19,9 +19,9 @@ public class UserAccountTest {
             "John", "Doe", "john.doe@example.com", "pass"
         );
         this.user = new User(userDetails);
-        this.userAccount = new UserAccount(user);
-        this.creditEvent = new TransactionEvent(1L, TransactionType.CREDIT, 150.0);
-        this.debitEvent = new TransactionEvent(2L, TransactionType.DEBIT, 50.0);
+        this.userAccount = new UserAccount(user, "USD");
+        this.creditEvent = new TransactionEvent(1L, TransactionType.CREDIT, 150.0, "2341", "USD");
+        this.debitEvent = new TransactionEvent(2L, TransactionType.DEBIT, 50.0, "2342", "USD");
     }
 
     // addTransactionEvent() tests ------------------------------------------------
@@ -50,7 +50,9 @@ public class UserAccountTest {
 
     @Test
     void testAddTransactionEvent_NullTransactionType() {
-        TransactionEvent event = new TransactionEvent(1L, null, 150.0);
+        TransactionEvent event = new TransactionEvent(
+            1L, null, 150.0, "2341", "USD"
+        );
         assertThrows(
             IllegalArgumentException.class,
             () -> this.userAccount.addTransactionEvent(event)
@@ -59,7 +61,9 @@ public class UserAccountTest {
 
     @Test
     void testAddTransactionEvent_NegativeAmount() {
-        TransactionEvent event = new TransactionEvent(1L, TransactionType.CREDIT, -150.0);
+        TransactionEvent event = new TransactionEvent(
+            1L, TransactionType.CREDIT, -150.0, "2341", "USD"
+        );
         assertThrows(
             IllegalArgumentException.class,
             () -> this.userAccount.addTransactionEvent(event)
@@ -68,7 +72,9 @@ public class UserAccountTest {
 
     @Test
     void testAddTransactionEvent_ZeroAmount() throws InsufficientBalanceException {
-        TransactionEvent event = new TransactionEvent(1L, TransactionType.CREDIT, 0.0);
+        TransactionEvent event = new TransactionEvent(
+            1L, TransactionType.CREDIT, 0.0, "2341", "USD"
+        );
         this.userAccount.addTransactionEvent(event);
         assertEquals(0.0, this.user.getAccountBalance());
         assertTrue(this.userAccount.getTransactionLog().contains(event));
@@ -78,10 +84,10 @@ public class UserAccountTest {
     @Test
     void testRecalculateBalance() throws InsufficientBalanceException {
         TransactionEvent creditEvent = new TransactionEvent(
-            System.currentTimeMillis(), TransactionType.CREDIT, 200.0
+            System.currentTimeMillis(), TransactionType.CREDIT, 200.0, "2341", "USD"
         );
         TransactionEvent debitEvent = new TransactionEvent(
-            System.currentTimeMillis(), TransactionType.DEBIT, 200.0
+            System.currentTimeMillis(), TransactionType.DEBIT, 200.0, "2342", "USD"
         );
         this.userAccount.addTransactionEvent(creditEvent);
         this.userAccount.addTransactionEvent(debitEvent);
@@ -120,7 +126,7 @@ public class UserAccountTest {
         );
         this.user.setAccountBalance(0.1);
         this.userAccount.addTransactionEvent(new TransactionEvent(
-            System.currentTimeMillis(), TransactionType.CREDIT, 0.0
+            System.currentTimeMillis(), TransactionType.CREDIT, 0.0, "2341", "USD"
         ));
         assertEquals(
             0.0, this.userAccount.recalculateBalance(),
@@ -132,7 +138,7 @@ public class UserAccountTest {
     // equals() tests ------------------------------------------------
     @Test
     void testEquals_SameUserAndTransactionLog() {
-        UserAccount anotherUserAccount = new UserAccount(user);
+        UserAccount anotherUserAccount = new UserAccount(user, "USD");
         anotherUserAccount.getTransactionLog().add(creditEvent);
         userAccount.getTransactionLog().add(creditEvent);
         assertEquals(userAccount, anotherUserAccount);
@@ -140,7 +146,7 @@ public class UserAccountTest {
 
     @Test
     void testEquals_SameUserReorderedTransactionLog() {
-        UserAccount anotherUserAccount = new UserAccount(user);
+        UserAccount anotherUserAccount = new UserAccount(user, "USD");
         anotherUserAccount.getTransactionLog().add(creditEvent);
         anotherUserAccount.getTransactionLog().add(debitEvent);
         userAccount.getTransactionLog().add(debitEvent);
@@ -150,14 +156,16 @@ public class UserAccountTest {
 
     @Test
     void testEquals_SameUserAndSameEmptyTransactionLog() {
-        UserAccount anotherUserAccount = new UserAccount(user);
+        UserAccount anotherUserAccount = new UserAccount(user, "USD");
         assertEquals(userAccount, anotherUserAccount);
     }
 
     @Test
     void testEquals_SameUserAndDifferentTransactionLog() {
-        UserAccount anotherUserAccount = new UserAccount(user);
-        TransactionEvent anotherCreditEvent = new TransactionEvent(3L, TransactionType.CREDIT, 200.0);
+        UserAccount anotherUserAccount = new UserAccount(user, "USD");
+        TransactionEvent anotherCreditEvent = new TransactionEvent(
+            3L, TransactionType.CREDIT, 200.0, "2341", "USD"
+        );
         anotherUserAccount.getTransactionLog().add(anotherCreditEvent);
         anotherUserAccount.getTransactionLog().add(debitEvent);
         assertNotEquals(userAccount, anotherUserAccount);
@@ -169,7 +177,7 @@ public class UserAccountTest {
             "John", "Doe", "john.doe@example.com", "pass"
         );
         User anotherUser = new User(anotherUserDetails);
-        UserAccount anotherUserAccount = new UserAccount(anotherUser);
+        UserAccount anotherUserAccount = new UserAccount(anotherUser, "USD");
         anotherUserAccount.addTransactionEvent(this.creditEvent);
         this.userAccount.addTransactionEvent(this.creditEvent);
         assertEquals(this.userAccount, anotherUserAccount);
@@ -178,7 +186,7 @@ public class UserAccountTest {
     @Test
     void testEquals_DifferentUser() {
         User anotherUser = new User(this.userDetails);
-        UserAccount anotherUserAccount = new UserAccount(anotherUser);
+        UserAccount anotherUserAccount = new UserAccount(anotherUser, "USD");
         anotherUserAccount.getTransactionLog().add(this.creditEvent);
         anotherUserAccount.getTransactionLog().add(this.debitEvent);
         assertNotEquals(this.userAccount, anotherUserAccount);
@@ -187,8 +195,10 @@ public class UserAccountTest {
     @Test
     void testEquals_DifferentUserAndTransactionLog() {
         User anotherUser = new User(this.userDetails);
-        UserAccount anotherUserAccount = new UserAccount(anotherUser);
-        TransactionEvent anotherCreditEvent = new TransactionEvent(3L, TransactionType.CREDIT, 200.0);
+        UserAccount anotherUserAccount = new UserAccount(anotherUser, "USD");
+        TransactionEvent anotherCreditEvent = new TransactionEvent(
+            3L, TransactionType.CREDIT, 200.0, "2341", "USD"
+        );
         anotherUserAccount.getTransactionLog().add(anotherCreditEvent);
         anotherUserAccount.getTransactionLog().add(debitEvent);
         assertNotEquals(userAccount, anotherUserAccount);
